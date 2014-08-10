72ddc69286f2d9939573c962b7b3dbbc5ac8f478
gehero <yang@Mac-10ddb1ef529e.local>
2014-08-11 02:15:33 +0800

    update some file

--- VSImageView/main.m
+++ VSImageView/main.m
@@ -13,6 +13,12 @@
 int main(int argc, char * argv[])
 {
     @autoreleasepool {
+//        for (int i = 0; i < 100; i++) {
+//            NSInteger x = arc4random_uniform(100);
+//            NSInteger y = arc4random_uniform(100) + 1000;
+//            CGFloat b = x / (CGFloat)y;
+//            NSLog(@"x:%i y:%i b:%f", x, y, b);
+//        }
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([VSAppDelegate class]));
     }
 }
