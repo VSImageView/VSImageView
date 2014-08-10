8cabc6682106f71ac0c8ebb4dbdcbf937c51023f
gehero <yang@Mac-10ddb1ef529e.local>
2014-08-10 23:11:49 +0800

    新增项目

---
+++ VSViewController.h
@@ -0,0 +1,13 @@
+//
+//  VSViewController.h
+//  VSImageView
+//
+//  Created by 杨进鹏 on 14/8/10.
+//  Copyright (c) 2014年 viplux. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VSViewController : UIViewController
+
+@end
---
+++ VSViewController.m
@@ -0,0 +1,28 @@
+//
+//  VSViewController.m
+//  VSImageView
+//
+//  Created by 杨进鹏 on 14/8/10.
+//  Copyright (c) 2014年 viplux. All rights reserved.
+//
+
+#import "VSViewController.h"
+
+@interface VSViewController ()
+
+@end
+
+@implementation VSViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+}
+
+@end
